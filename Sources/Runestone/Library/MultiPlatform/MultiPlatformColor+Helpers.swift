9d87d42e3b7b5b28a976c9451532b5ab9f12640f
dontwanttothink <barquean.09excavacion@icloud.com>
2025-10-21 09:30:37 -0500

    .

--- Sources/Runestone/Library/MultiPlatform/MultiPlatformColor+Helpers.swift
+++ Sources/Runestone/Library/MultiPlatform/MultiPlatformColor+Helpers.swift
@@ -1,9 +1,17 @@
+import Foundation
+
+func debugAssetLoading() {
+    print("Bundle path: \(Bundle.module.bundlePath)")
+    print("Resource path: \(Bundle.module.resourcePath ?? "nil")")
+}
+
 extension MultiPlatformColor {
     convenience init(themeColorNamed name: String) {
         let fullName = "theme_" + name
         #if os(iOS) || os(xrOS)
         self.init(named: fullName, in: .module, compatibleWith: nil)!
         #else
+        debugAssetLoading()
         self.init(named: fullName, bundle: .module)!
         #endif
     }
